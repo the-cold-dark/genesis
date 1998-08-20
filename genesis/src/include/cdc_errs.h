331aae6543603de1affae38e86c7f26af019a282
bruce <bruce>
1998-08-20 07:19:34 +0000

    Defines for nonblocking I/O fixes

--- genesis/src/include/cdc_errs.h
+++ genesis/src/include/cdc_errs.h
@@ -17,6 +17,7 @@
 #define     ERR_NETUNREACH   WSAENETUNREACH
 #define     ERR_TIMEDOUT     WSAETIMEDOUT
 #define     ERR_CONNREFUSED  WSAECONNREFUSED
+#define     ERR_AGAIN        WSAWOULDBLOCK
 
 #define GETERR()      GetLastError()
 #define SETERR(_err_) SetLastError(_err_)
@@ -32,6 +33,7 @@
 #define     ERR_NETUNREACH   ENETUNREACH
 #define     ERR_TIMEDOUT     ETIMEDOUT
 #define     ERR_CONNREFUSED  ECONNREFUSED
+#define     ERR_AGAIN        EAGAIN
 
 #define GETERR()      errno
 #define SETERR(_err_) errno = _err_

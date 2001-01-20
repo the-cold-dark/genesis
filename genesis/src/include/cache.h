95e5634a4dc86edb45274349dfc69d3fd6b70371
braddr <braddr>
2001-01-20 05:22:28 +0000

    properly define cache_dirty_object as inline

--- genesis/src/include/cache.h
+++ genesis/src/include/cache.h
@@ -12,6 +12,7 @@
 
 
 void init_cache(Bool spawn_cleaner);
+inline void cache_dirty_object(Obj *obj);
 Obj *cache_get_holder(Long objnum);
 Obj *cache_retrieve(Long objnum);
 Obj *cache_grab(Obj *object);

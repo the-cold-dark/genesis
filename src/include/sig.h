4508bcecbe8f85d52c51055ec76e059c99d45fb8
Bruce Mitchener <bruce.mitchener@gmail.com>
2022-12-09 22:16:24 +0700

    Flag `dump_core_and_exit` and `panic` as `noreturn`.
    Now that we know this works for our builds, we can do more of this.

--- src/include/sig.h
+++ src/include/sig.h
@@ -5,13 +5,15 @@
 #ifndef cdc_signal_h
 #define cdc_signal_h
 
+#include <stdnoreturn.h>
+
 void init_sig(void);
 
 extern short caught_fpe;   /* if we catch SIGFPE */
 
 /* void catch_signal(int sig, int code, struct sigcontext *scp); */
 void catch_signal(int sig);
-void dump_core_and_exit(void);
+noreturn void dump_core_and_exit(void);
 
 #endif
 
